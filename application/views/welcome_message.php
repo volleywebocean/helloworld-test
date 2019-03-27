f809d35dd15eb867ddfe5a441a4ca6a686eafc49
volleywebocean <volleysport1234@outlook.com>
2019-03-28 01:26:16 +0300

    init

---
+++ application/views/welcome_message.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="text-center">Hello World!</div>
+		</div>
+	</div>
+</body>
+</html>
